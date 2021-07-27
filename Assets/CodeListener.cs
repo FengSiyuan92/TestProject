32434a989641ed4346081ad9b817794baa95930e
fengsiyuan@onemt.com.cn <fengsiyuan@onemt.com.cn>
2021-07-27 14:19:21 +0800

    init

---
+++ Assets/CodeListener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XLua;
+
+public class CodeListener : MonoBehaviour
+{
+    InputField input;
+
+    LuaEnv env = new LuaEnv();
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<InputField>();
+        env.DoString(@"
+Log = CS.UnityEngine.Debug.Log
+LogError = CS.UnityEngine.Debug.LogError
+Application = CS.UnityEngine.Application
+
+"
+);
+        
+    }
+
+    public void RunCode()
+    {
+        env.DoString(input.text);
+    }
+
+    public void Clear()
+    {
+        input.text = "";
+    }
+}
---
+++ Assets/LookLog.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LookLog : MonoBehaviour
+{
+
+    Text text;
+    string content = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        Application.logMessageReceived += CollectLog;
+        text.text = content;
+    }
+
+    void CollectLog(string condition, string stackTrace, LogType type)
+    {
+        if (type == LogType.Error)
+        {
+            content += "\n Error:" + condition;
+        }
+        else if (type == LogType.Exception)
+        {
+            content += "\n Exception:" + condition  ;
+        }
+        else if (type == LogType.Log)
+        {
+            content += "\n Log:" + condition ;
+        }
+
+        text.text = content;
+    }
+
+    public void Clear()
+    {
+        content = "";
+        text.text = content;
+    }
+
+}
